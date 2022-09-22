608b24c543d903a6775b57f8971ad1663de9dc3d
ndkhangvl <nguyenduykhang410@gmail.com>
2022-09-22 08:18:38 +0700

    Configure ESLinit for the project

--- src/controllers/contact.controller.js
+++ src/controllers/contact.controller.js
@@ -25,3 +25,24 @@ exports.deleteAll = (req, res) => {
 exports.findAllFavorite = (req, res) => {
     return res.send({ message: 'findAllFavorite handler'})
 }
+
+const ContactService = require('../services/contact.service');
+const ApiError = require('../api-error');
+const ContactService = require('../services/contact.service');
+
+exports.create = async(req, res, next) => {
+    if(!req.body?.name) {
+        return next(new ApiError(400, 'Name can not be empty'));
+    }
+
+    try {
+        const ContactService = new ContactService();
+        const contact = await ContactService.create(req.body);
+        return res.send(contact);        
+    } catch (error) {
+        console.log(error);
+        return next(
+            new ApiError(500, ' An error occured while creating the contact')
+        );
+    }
+}
